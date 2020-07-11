a5d3164cc90d2abb289a488aa6e95478e308b3b0
kriss12n <kriss12n@gmail.com>
2020-07-11 04:15:14 -0400

    docs

---
+++ docs/precache-manifest.def107f72a22aecd94beac112196fc8f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc1647114b27733eb3b3f97682cef1aa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f1174ea92ea551cb107b",
+    "url": "/static/css/main.a1c1feb8.chunk.css"
+  },
+  {
+    "revision": "496f528a92b3c1e745d3",
+    "url": "/static/js/2.6d4f56ca.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6d4f56ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1174ea92ea551cb107b",
+    "url": "/static/js/main.eb5ca931.chunk.js"
+  },
+  {
+    "revision": "e5ee6826b74226f47af5",
+    "url": "/static/js/runtime-main.b577d66e.js"
+  }
+]);
